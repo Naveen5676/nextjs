fd5b29c88955cd282790056c505df8364549b853
Naveen5676 <sample.first@yahoo.com>
2023-12-28 09:04:24 +0530

    implementing  page routing

---
+++ src/Pages/aboutus.js
@@ -0,0 +1,4 @@
+const aboutus =()=>{
+ return <h1>the about us page</h1>
+}
+export default aboutus
---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+// our-domain/
+
+const HomePage=()=>{
+    return <h1>The HomePage</h1>
+}
+export default HomePage
