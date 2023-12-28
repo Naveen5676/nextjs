cff892df6d10da4b461b00b130daa61c05700192
Naveen5676 <sample.first@yahoo.com>
2023-12-28 19:10:46 +0530

    Final touch to news page

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,21 @@
 // our-domain/news
-
+import Link from 'next/link';
+import { Fragment } from 'react';
 const NewsHomePage=()=>{
-    return <h1>The Home News Page</h1>
+    return (
+        <Fragment>
+    <h1>The Home News Page</h1>
+    <ul>
+        <li>
+            <Link href='/news/1'>Link to id 1</Link>
+        </li>
+        <li>
+            <Link href='/news/2'>Link to id 2</Link>
+        </li>
+        <li>
+            <Link href='/news/3'>Link to id 3</Link>
+        </li>
+    </ul>
+    </Fragment>);
 }
 export default NewsHomePage
